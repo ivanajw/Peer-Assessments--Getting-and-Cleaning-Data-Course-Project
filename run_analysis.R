4f46302b28d01a78f9f97ea286e8f67439d284a8
Ivana Williams <ivana.jelic.williams@gmail.com>
2014-06-22 15:31:18 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -35,20 +35,20 @@ y_train <- read.table(paste0(dataTrainDirectory, "y_train.txt"), header=FALSE)
 tmp <- data.frame(Activity = factor(y_train$V1, labels = activities$V2))
 trainData <- cbind(tmp, subject_train, x_train)
 
-# Preparing Tidy Data from mean() and std() data.
+# Preparing tidy data from mean() and std() data.
 tmpTidyData <- rbind(testData, trainData)
 names(tmpTidyData) <- c("Activity", "Subject", features[,2])
 select <- features$V2[grep("mean\\(\\)|std\\(\\)", features$V2)]
 tidyData <- tmpTidyData[c("Activity", "Subject", select)]
 
-# Write Tidy Data to Disk in tidyData.txt 
+# Write tidy data to disk in tidyData.txt 
 write.table(tidyData, file="./tidyData.txt", row.names=FALSE)
 
-# Tidy Data Average/Activity. Melt and Cast
+# Tidy data average/activity. melt and cast
 tidyDataMelt <- melt(tidyData, id=c("Activity", "Subject"), measure.vars=select)
 tidyDataMean <- dcast(tidyDataMelt, Activity + Subject ~ variable, mean)
 
-# Write Tidy Average Data to tidyAverageData.txt
+# Write tidy average data to tidyAverageData.txt
 write.table(tidyDataMean, file="./tidyAverageData.txt", row.names=FALSE)
 
 
